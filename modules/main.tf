bff17f65c3a245c9632e8241bcccfc4d59586f07
Mohammed Maaz <mohmmedmaaz57@gmail.com>
2026-08-31 00:55:34 +0530

    proper child and parent modulr

--- modules/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.rg
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "rg prod"
-  location = "West Europe"
-}
--- modules/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "rg" {
-  default = "rgtest"
-}
